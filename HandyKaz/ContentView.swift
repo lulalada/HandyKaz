7b017a569ed25482f95cc2118d6c2580ef9e005e
Alua <sayabayeva.alua@gmail.com>
2024-01-04 00:43:15 +0600

    Initial Commit

---
+++ HandyKaz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HandyKaz
+//
+//  Created by Alua Sayabayeva on 04/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HandyKaz/HandyKazApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandyKazApp.swift
+//  HandyKaz
+//
+//  Created by Alua Sayabayeva on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct HandyKazApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
